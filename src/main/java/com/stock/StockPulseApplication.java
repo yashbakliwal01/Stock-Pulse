c794864b0cee402d8a36f4a79ad0ee391e8b647e
yashbakliwal01 <yashbakliwal01@gmail.com>
2025-02-24 19:49:37 +0530

    first commit

--- src/main/java/com/stock/StockPulseApplication.java
+++ src/main/java/com/stock/StockPulseApplication.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan("com.stock")
 public class StockPulseApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(StockPulseApplication.class, args);
 	}
